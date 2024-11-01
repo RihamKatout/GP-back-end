38253c80d2b55854f266280e99577e56bcce737d
rihamkatout <rihamkatm@gmail.com>
2024-11-02 01:11:19 +0200

    init: initialize spring boot project

---
+++ src/test/java/gp/riham_aisha/back_end/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package gp.riham_aisha.back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
